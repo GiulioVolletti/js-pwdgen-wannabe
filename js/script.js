379c8baf012dd590defaa25582f9e60c5b3f6ec7
Giulio <giuliovolletti@hotmail.com>
2020-11-10 15:20:35 +0100

    commit 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// dati per la password
+// nome
+var nome = prompt("inserire falsissimo nome");
+console.log(nome);
+// cognome
+var cognome = prompt("inserire cognome");
+console.log(cognome);
+// colore
+var color = prompt("inserire colore");
+console.log(color);
+
+
+// passowrd
+document.getElementById('password').innerHTML= nome + cognome + color + "19"
+console.log(password);
